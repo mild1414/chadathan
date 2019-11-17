ec4c29b08d42a73cefce5665926ee8fbf496a943
mild1414 <57699648+mild1414@users.noreply.github.com>
2019-11-18 00:29:18 +0700

    Update 6.go

--- 6.go
+++ 6.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println('\n \t Bachticks')
+func main() {
+	fmt.Println(`\n \t Bachticks`)
 	fmt.Println("\n \t Double quote")
 	fmt.Println()
-	fmt.Println('\n \t Backticks'[0])
-	fmt.Println(len('n \t Backticks'))
-	fmt.Println("Hello"+"World")
+	fmt.Println(`\n \t Backticks`[0])
+	fmt.Println(len(`n \t Backticks`))
+	fmt.Println("Hello" + "World")
 }
