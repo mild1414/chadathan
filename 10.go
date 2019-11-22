af567e0541dfa47a59c9c14056b437a8de34c360
mild1414 <57699648+mild1414@users.noreply.github.com>
2019-11-22 15:13:18 +0700

    Update 10.go

--- 10.go
+++ 10.go
@@ -1,7 +1,20 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
 func main() {
 	startA := time.Now()
+	var a string
+	for i := 0; i < 10000; i++ {
+		a += "a"
+	}
+	fmt.Println("a", time.Since(startA))
+
+	
+
+	}
 }
