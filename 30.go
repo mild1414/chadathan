2a554a92f5aadba03964f47f1c3c50ca76809f87
mild1414 <57699648+mild1414@users.noreply.github.com>
2019-11-30 19:09:59 +0700

    Update 30.go

--- 30.go
+++ 30.go
@@ -8,5 +8,7 @@ func main() {
 	fmt.Printf("type = %T \n", 3.14159265359)
 	fmt.Printf("pi = %f \n", 3.14159265359)
 	fmt.Sprintf("pi = %2.2f \n", 3.14159265359)
-	fmt.Printf("pi = 59.f \n", 3.14159265359)
+	fmt.Printf("pi = %9.f \n", 3.14159265359)
+	fmt.Printf("pi = %-9.f \n", 3.14159265359)
+	fmt.Println("pi = %09.f \n", 3.14159265359)
 }
