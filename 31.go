53a5fb39817cb06148615fa2dec01706f6a55654
mild1414 <57699648+mild1414@users.noreply.github.com>
2019-11-30 19:34:50 +0700

    Update 31.go

--- 31.go
+++ 31.go
@@ -5,4 +5,6 @@ import "fmt"
 func main() {
 	n, e := fmt.Println("Hello", "World", 123, 456, "\n")
 	fmt.Print("number of bytes written :", n, "\n")
+	fmt.Print("write error encountered :", e, "\n")
+
 }
