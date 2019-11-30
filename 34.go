e0ec62491fdf4495adf10e82a0dd5b2c1ab83b99
mild1414 <57699648+mild1414@users.noreply.github.com>
2019-11-30 20:46:47 +0700

    Update 34.go

--- 34.go
+++ 34.go
@@ -10,4 +10,6 @@ func main() {
 	var weight float32
 	n, err := fmt.Scan(&name, &age, &weigth, &heigth)
 	fmt.Println(name, age, weight, height)
+	fmt.Println(`number of argument `, n)
+	fmt.Println(`error `, err)
 }
