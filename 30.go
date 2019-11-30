43297a2de4b0b9caf55e2e576114d70024dc7ffc
mild1414 <57699648+mild1414@users.noreply.github.com>
2019-11-30 19:17:53 +0700

    Update 30.go

--- 30.go
+++ 30.go
@@ -13,4 +13,10 @@ func main() {
 	fmt.Println("pi = %09.f \n", 3.14159265359)
 	fmt.Println("pi = %09.2f \n", 3.14159265359)
 	fmt.Printf("pi = %E \n", 3.14159265359)
+
+	fmt.Printf("1 > 2 = %t \n", 1 > 2)
+	fmt.Printf("10 (base 2) = %b \n", 10)
+	fmt.Println("10 (base 8) = %o \n", 10)
+	fmt.Printf("10 (base 10) = %d \n", 10)
+	fmt.Printf("10 (base 16) = %x \n", 10)
 }
