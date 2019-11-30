3971453a211d4cb55990704976310904aad2ca04
mild1414 <57699648+mild1414@users.noreply.github.com>
2019-11-30 19:04:45 +0700

    Update 30.go

--- 30.go
+++ 30.go
@@ -1,1 +1,11 @@
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("My name is %s, i am %d year old\n", "Goku", 18)
+
+	fmt.Printf("type = %T \n", 3.14159265359)
+	fmt.Printf("pi = %f \n", 3.14159265359)
+	fmt.Sprintf("pi = %2.2f \n", 3.14159265359)
+}
