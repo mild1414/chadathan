f2b4325788ce8016335fb4a1922798eab016475f
mild1414 <57699648+mild1414@users.noreply.github.com>
2019-12-22 20:38:11 +0700

    Update 53.go

--- 53.go
+++ 53.go
@@ -5,6 +5,6 @@ import "fmt"
 func main() {
 	a := []string{"A", "B", "C", "D", "E"}
 	deleteIndex := 2
-	a = append(a[:deleteIndex], a[deleteIndex+1]...)
+	a = append(a[:deleteIndex], a[deleteIndex+1:]...)
 	fmt.Println(a)
 }
