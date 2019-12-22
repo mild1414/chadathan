2b29b3c2cfc15e7dc5f63c0ad825e4711928b3a1
mild1414 <57699648+mild1414@users.noreply.github.com>
2019-12-22 22:23:27 +0700

    Update 58.go

--- 58.go
+++ 58.go
@@ -1,7 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
 	elements := make(map[string]map[string]string)
 	elements["H"] = map[string]string{"name": "Hydrogen", "state": "gas"}
 	elements["He"] = map[string]string{"name": "Helium", "state": "gas"}
+	fmt.Println(elements)
 }
