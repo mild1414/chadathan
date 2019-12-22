8fb54d34d975587f7b6bd03afb5e3881a844415c
mild1414 <57699648+mild1414@users.noreply.github.com>
2019-12-22 22:53:58 +0700

    Update 60.go

--- 60.go
+++ 60.go
@@ -1,8 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 	elements := make(map[string]string)
 	elements["H"] = "Hydrogen"
 	elements["He"] = "Helium"
 	elements["Li"] = "Lithium"
+	fmt.Println(elements)
 }
