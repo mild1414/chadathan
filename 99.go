95a8364a3196808aaf406a4124ca57ae9662e69c
mild1414 <57699648+mild1414@users.noreply.github.com>
2020-01-18 20:42:14 +0700

    Update 99.go

--- 99.go
+++ 99.go
@@ -1,9 +1,13 @@
 package main
 
+import "fmt"
+
 type student struct {
 	name  string
 	age   int
 	email string
 }
 
-func (std student) introduce() {}
+func (std student) introduce() {
+	fmt.Println()
+}
