371f42a059adb5f12e8e834ba4d26d341bb5f71f
mild1414 <57699648+mild1414@users.noreply.github.com>
2020-01-25 20:50:32 +0700

    Update 118.go

--- 118.go
+++ 118.go
@@ -16,5 +16,5 @@ func increment(data *int, mutex *sync.Mutex, wg *sync.WaitGroup) {
 func read(data *int, mutex *sync.Mutex, wg *sync.WaitGroup){
 	start := time.Now()
 	defer wg.Done()
-	defer 
+	defer mutex.Unlock()
 }
