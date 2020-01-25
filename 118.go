0d8844f2ded13f3692c10f924607189825b2aad8
mild1414 <57699648+mild1414@users.noreply.github.com>
2020-01-25 20:56:26 +0700

    Update 118.go

--- 118.go
+++ 118.go
@@ -20,4 +20,6 @@ func read(data *int, mutex *sync.Mutex, wg *sync.WaitGroup){
 	mutex.Lock()
 	fmt.Println(time.Sinc(start), "Data =", *data)
 }
-func main(){}
+func main(){
+	var mutex sync.Mutex
+}
