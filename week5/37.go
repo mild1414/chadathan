c33f6fc594fa8b1c3a85f3d4a6bbaa62872bee96
mild1414 <57699648+mild1414@users.noreply.github.com>
2020-01-27 00:27:26 +0700

    Create 37.go

---
+++ week5/37.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	student_1 := "Goku"
+	student_2 := "Gohan"
+	student_3 := "Vegeta"
+	student_4 := "Piccolo"
+	student_5 := "Krillin"
+	student_6 := "Yamcha"
+	student_7 := "Bluma"
+	student_8 := "Videl"
+	student_9 := "Oolong"
+	student_10 := "Puar"
+
+	fmt.Println(student_1, student_2, student_3, student_4, student_5, student_6, student_7, student_8, student_9, student_10)
+}
