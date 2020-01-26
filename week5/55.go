02fde891979fd8106638a4e304f994b96bd208b5
mild1414 <57699648+mild1414@users.noreply.github.com>
2020-01-27 00:28:23 +0700

    Create 55.go

---
+++ week5/55.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	a := []int{1, 2, 3}
+	b := []int{1, 2, 3}
+	fmt.Println(reflect.DeepEqual(a, b))
+	c := []string{"hi", "hello"}
+	d := []string{"hello", "hi"}
+	fmt.Println(reflect.DeepEqual(c, d))
+
+}
