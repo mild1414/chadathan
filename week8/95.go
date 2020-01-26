76af0e07d801b8093528800cd76703c195270c7b
mild1414 <57699648+mild1414@users.noreply.github.com>
2020-01-27 00:29:35 +0700

    Create 95.go

---
+++ week8/95.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type student struct {
+	name  string
+	age   int
+	email string
+}
+
+func main() {
+	var a student
+	a.name = "Goku"
+	a.age = 18
+	a.email = "Goku@super.saiya"
+
+	b := student{"Gohan", 3, "Gohan@super.saiya"}
+
+	c := student{name: "Videl", email: "Videl@daughter.satan"}
+
+	d := student{age: 20}
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
