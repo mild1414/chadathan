472c293a6657a17f5effc84fc578aa2593a8266b
mild1414 <57699648+mild1414@users.noreply.github.com>
2020-01-27 00:31:37 +0700

    Create 113.go

---
+++ week11/113.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func say(text string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(i, ":", text)
+	}
+}
+
+func main() {
+	go say("Hello")
+	go say("Hi")
+	var input string
+	fmt.Println(&input)
+}
