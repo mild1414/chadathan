2e1582181bbf9b4836bf4fa76b0037f33d01000e
mild1414 <57699648+mild1414@users.noreply.github.com>
2020-01-27 00:31:12 +0700

    Create 122.go

---
+++ week11/122.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func sum(c chan int, number ...int) {
+	sum := 0
+	for _, v := range number {
+		sum = sum + v
+	}
+	c <- sum
+}
+
+func printer(c chan int) {
+	number := <-c
+	fmt.Println(number)
+}
+
+func main() {
+	c := make(chan int)
+	go printer(c)
+	go printer(c)
+	go sum(c, 1, 2, 3)
+	go sum(c, 10, 11)
+
+	var input string
+	fmt.Scan(&input)
+}
