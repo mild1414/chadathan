f846ee1d7687fbb04b512e4c9b0b2276831f9d59
mild1414 <57699648+mild1414@users.noreply.github.com>
2020-01-30 20:36:19 +0700

    Create 107.go

---
+++ week9/107.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type myError struct {
+	error string
+}
+
+func (e myError) Error() string {
+	return e.error
+}
+func say(word string) error {
+	if word == "hi" {
+		return myError{"can't say hi"}
+	}
+	return nil
+}
+func main() {
+	e1 := say("hello")
+	fmt.Println(e1)
+	e2 := say("hi")
+	fmt.Println(e2)
+}
