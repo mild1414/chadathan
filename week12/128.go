9dc67f9e731b4357d89400aa3f792370e32346b4
mild1414 <57699648+mild1414@users.noreply.github.com>
2020-01-31 20:58:33 +0700

    Update 128.go

--- week12/128.go
+++ week12/128.go
@@ -1,11 +1,16 @@
 package main
 
-import "strings"
+import (
+	"io"
+	"strings"
+)
 
 func main() {
 	reader := strings.NewReader("HelloWorld")
 	p := make([]byte, 3)
 	for {
 		n, err := reader.read(p)
+		if err == io.EOF {
+		}
 	}
 }
