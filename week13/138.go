66e763ad53564fcf6fde506f9f7326161efdae1c
mild1414 <57699648+mild1414@users.noreply.github.com>
2020-02-09 20:45:00 +0700

    Update 138.go

--- week13/138.go
+++ week13/138.go
@@ -18,5 +18,6 @@ func main() {
 	fmt.Println(person{"Bob", 20})
 	fmt.Println(person{name: "Alice", age: 30})
 	fmt.Println(person{name: "Fred"})
-	fmt.Println(person{name: "Ann", age: 40})
+	fmt.Println(&person{name: "Ann", age: 40})
+	fmt.Println(NewPerson("Jon"))
 }
