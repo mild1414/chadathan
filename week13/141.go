0a9ecc44c7a8389cdb3c7a324bd5d8507980408d
mild1414 <57699648+mild1414@users.noreply.github.com>
2020-02-14 00:55:46 +0700

    Update 141.go

--- week13/141.go
+++ week13/141.go
@@ -1,15 +1,1 @@
 package main
-
-import (
-	"fmt"
-)
-
-type Employee struct {
-	firstName, lastName string
-	age, salary         int
-}
-
-funt main(){
-	var emp4 Employee
-	fmt.Println("Employee 4", emp4)
-}
