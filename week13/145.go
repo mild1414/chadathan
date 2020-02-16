d7574b0a52ebf049903bb97a2f3111205e82d8db
mild1414 <57699648+mild1414@users.noreply.github.com>
2020-02-16 22:19:40 +0700

    Update 145.go

--- week13/145.go
+++ week13/145.go
@@ -6,9 +6,16 @@ func main() {
 	fmt.Print("input : ")
 	var water float32
 	var fire float32
+	var totel float32
 	n, err := fmt.Scan(&water, &fire)
 	water = water * 1
 	fire = fire * 3
 	fmt.Println("water : %f", water)
 	fmt.Println("fire : %f", fire)
+
+	water = water + ((water * 7) / 100)
+	fire = fire + ((fire * 7) / 100)
+	totel = water + fire
+	fmt.Printf("ภาษี %f", water)
+
 }
