72d575ba9bc49fb320e2115e264115aaa7734952
mild1414 <57699648+mild1414@users.noreply.github.com>
2020-02-17 00:56:27 +0700

    Create 6.go

---
+++ week1/6.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(`\n \t Bachticks`)
+	fmt.Println("\n \t Double quote")
+	fmt.Println()
+	fmt.Println(`\n \t Backticks`[0])
+	fmt.Println(len(`n \t Backticks`))
+	fmt.Println("Hello" + "World")
+}
