831c3f9298558951c0078feacd1dc83eddc179cd
mild1414 <57699648+mild1414@users.noreply.github.com>
2020-02-17 00:54:44 +0700

    Create 80.go

---
+++ week8/80.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func say(greet string) func(string) string {
+	return func(name string) string {
+		return greet + name
+	}
+}
+
+func main() {
+	x := say("Hello")
+
+	fmt.Println(x("Goku"))
+	fmt.Println(x("Gohan"))
+}
