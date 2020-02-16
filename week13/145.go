4926b81b4392d1ed09f419ad16f2d51bf9b045c3
mild1414 <57699648+mild1414@users.noreply.github.com>
2020-02-16 21:43:21 +0700

    Update 145.go

--- week13/145.go
+++ week13/145.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("input : ")
+	fmt.Print("input : ", water)
+	fmt.Print("input : ", fire)
 	var water float32
 	var fire float32
-	fmt.Scanf()
+	fmt.Scanf(&water, &fire)
 
 }
