3e984ad26aa192e484ed0edef4e2f6494c1bf3b0
mild1414 <57699648+mild1414@users.noreply.github.com>
2020-02-17 00:48:50 +0700

    Create 126.go

---
+++ week13/126.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func generateInt(min, max int, ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := min; i <= max; i++ {
+		ch <- i
+	}
+}
+func main() {
+	ch := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go generateInt(10, 20, ch, &wg)
+	go generateInt(50, 200, ch, &wg)
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+	sum := 0
+	for i := range ch {
+		sum += i
+	}
+	fmt.Println(sum)
+}
